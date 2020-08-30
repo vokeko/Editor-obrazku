e60410b5056b99ffe764f13377c35e4e9b8e46dc
Vojtěch Bukovský <vojbukovsky@gmail.com>
2020-08-30 12:32:13 +0200

    Úvodní commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Editor_obrázků
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tlacitko_ulozit = new System.Windows.Forms.Button();
+            this.tlacitko_otevrit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.posuvnik = new System.Windows.Forms.TrackBar();
+            this.seznamEfektu = new System.Windows.Forms.ComboBox();
+            this.tlacitko_proved = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.posuvnik)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tlacitko_ulozit);
+            this.groupBox1.Controls.Add(this.tlacitko_otevrit);
+            this.groupBox1.Location = new System.Drawing.Point(1332, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(138, 216);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ovládání";
+            // 
+            // tlacitko_ulozit
+            // 
+            this.tlacitko_ulozit.Enabled = false;
+            this.tlacitko_ulozit.Location = new System.Drawing.Point(6, 58);
+            this.tlacitko_ulozit.Name = "tlacitko_ulozit";
+            this.tlacitko_ulozit.Size = new System.Drawing.Size(124, 31);
+            this.tlacitko_ulozit.TabIndex = 3;
+            this.tlacitko_ulozit.Text = "Uložit";
+            this.tlacitko_ulozit.UseVisualStyleBackColor = true;
+            this.tlacitko_ulozit.Click += new System.EventHandler(this.tlacitko_ulozit_Click);
+            // 
+            // tlacitko_otevrit
+            // 
+            this.tlacitko_otevrit.Location = new System.Drawing.Point(6, 21);
+            this.tlacitko_otevrit.Name = "tlacitko_otevrit";
+            this.tlacitko_otevrit.Size = new System.Drawing.Size(124, 31);
+            this.tlacitko_otevrit.TabIndex = 2;
+            this.tlacitko_otevrit.Text = "Otevřít";
+            this.tlacitko_otevrit.UseVisualStyleBackColor = true;
+            this.tlacitko_otevrit.Click += new System.EventHandler(this.tlacitko_otevrit_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.posuvnik);
+            this.groupBox2.Controls.Add(this.seznamEfektu);
+            this.groupBox2.Controls.Add(this.tlacitko_proved);
+            this.groupBox2.Location = new System.Drawing.Point(1332, 234);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(138, 707);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Efekty";
+            // 
+            // posuvnik
+            // 
+            this.posuvnik.Location = new System.Drawing.Point(9, 58);
+            this.posuvnik.Maximum = 50;
+            this.posuvnik.Name = "posuvnik";
+            this.posuvnik.Size = new System.Drawing.Size(121, 56);
+            this.posuvnik.TabIndex = 2;
+            this.posuvnik.TickFrequency = 10;
+            this.posuvnik.Visible = false;
+            // 
+            // seznamEfektu
+            // 
+            this.seznamEfektu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.seznamEfektu.Enabled = false;
+            this.seznamEfektu.FormattingEnabled = true;
+            this.seznamEfektu.Items.AddRange(new object[] {
+            "Invertovat barvy",
+            "Rozmazat",
+            "Zesvětlit",
+            "Ztmavit"});
+            this.seznamEfektu.Location = new System.Drawing.Point(9, 120);
+            this.seznamEfektu.Name = "seznamEfektu";
+            this.seznamEfektu.Size = new System.Drawing.Size(121, 24);
+            this.seznamEfektu.TabIndex = 2;
+            this.seznamEfektu.SelectedIndexChanged += new System.EventHandler(this.seznamEfektu_SelectedIndexChanged);
+            // 
+            // tlacitko_proved
+            // 
+            this.tlacitko_proved.Enabled = false;
+            this.tlacitko_proved.Location = new System.Drawing.Point(9, 21);
+            this.tlacitko_proved.Name = "tlacitko_proved";
+            this.tlacitko_proved.Size = new System.Drawing.Size(121, 31);
+            this.tlacitko_proved.TabIndex = 4;
+            this.tlacitko_proved.Text = "Proveď";
+            this.tlacitko_proved.UseVisualStyleBackColor = true;
+            this.tlacitko_proved.Click += new System.EventHandler(this.tlacitko_proved_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 953);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Editor obrázků";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.posuvnik)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button tlacitko_ulozit;
+        private System.Windows.Forms.Button tlacitko_otevrit;
+        private System.Windows.Forms.Button tlacitko_proved;
+        private System.Windows.Forms.ComboBox seznamEfektu;
+        private System.Windows.Forms.TrackBar posuvnik;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor_obrázků
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.seznamEfektu.Items.Add("Otočit vodorovně");
+            this.seznamEfektu.Items.Add("Otočit svisle");
+        }
+        Bitmap obrazek;
+        //uchovává vybraný obrázek
+        Color[,] barvy;
+        //Array uchovává hodnoty jednotlivých pixelů 
+        int maxVyska = 750;
+        int maxSirka = 975;
+        //maxVyska a sirka určují maximální rozměry nahraného obrázku - pokud budou větší, obrázek se zmenší
+
+        private void otevriSoubor()
+        {
+            OpenFileDialog otevreniSouboru = new OpenFileDialog();
+            //openfiledialog = otevření rozhraní pro výběr souborů
+            otevreniSouboru.Filter = "Obrázky (*.bmp, *.jpg) | *.bmp;*.jpg;";
+            //.filter filtruje možné soubory, které lze vybrat
+            otevreniSouboru.ShowDialog();
+            if (otevreniSouboru.FileName != "")
+            {
+                try
+                {
+                    using (Bitmap docasnyObrazek = (Bitmap)Bitmap.FromFile(otevreniSouboru.FileName))
+                    {
+                        //using je použité pro dočasnou proměnnou (docasnyobrazek) - jakmile using skončí tak zmizí.
+                        if (obrazek != null)
+                        {
+                            //pokud už je použitý obrazek (tj. - byl předtím nahraný), tak se vymaže
+                            obrazek.Dispose();
+                            obrazek = null;
+                        }
+                        if (docasnyObrazek.Width > maxSirka || docasnyObrazek.Height > maxVyska)
+                        {
+                            //pokud je obrázek větší než rozměry, tak se přizpůsobí a naklonuje
+                            using (Bitmap docasnyObrazek2 = (Bitmap)ScaleImage(docasnyObrazek))
+                            {
+                                obrazek = (Bitmap)docasnyObrazek2.Clone();
+                            }
+                        }
+                        else
+                        {
+                            //pokud je obrázek menší než rozměry, tak se naklonuje a nemění se rozměry
+                            obrazek = (Bitmap)docasnyObrazek.Clone();
+                        }
+                        barvy = new Color[obrazek.Width, obrazek.Height];
+                        for (int sloupec = 0; sloupec < obrazek.Width; sloupec++)
+                        {
+                            for (int radek = 0; radek < obrazek.Height; radek++)
+                            {
+                                barvy[sloupec, radek] = obrazek.GetPixel(sloupec, radek);
+                            }
+                        }
+                        //pole barvy se obsadí jednotlivými pixely dle barev
+                    }
+                    seznamEfektu.Enabled = true;
+                    tlacitko_ulozit.Enabled = false;
+                    this.Invalidate();
+                    //invalidate = překreslení povrchu
+                }
+                catch (OutOfMemoryException)
+                {
+                    MessageBox.Show("Obrázek má moc velkou velikost!");
+                }
+
+            }
+        }
+
+        private void tlacitko_otevrit_Click(object sender, EventArgs e)
+        {
+            otevriSoubor();
+            //zavolání funkce při otevření souboru
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //zavolá se při invalidate - když se kreslí na formulář
+            if (obrazek != null)
+            {
+                e.Graphics.DrawImage(obrazek, 10, 10);
+            }
+
+        }
+        private Bitmap ScaleImage(Bitmap bmp)
+        {
+            //funkce se zavolá, když je obrázek větší než maximální rozměry a zmenší se v zachovaném poměru. Navrátí pozměněný obrázek.
+            var ratioX = (double)maxSirka / bmp.Width;
+            var ratioY = (double)maxVyska / bmp.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(bmp.Width * ratio);
+            var newHeight = (int)(bmp.Height * ratio);
+
+            var newImage = new Bitmap(bmp, newWidth, newHeight);
+
+            return newImage;
+        }
+        private void InverzeBarev()
+        {
+            for (int sloupec = 0; sloupec < obrazek.Width; sloupec++)
+            {
+
+                for (int radek = 0; radek < obrazek.Height; radek++)
+                {
+                    Color docasnaBarva = barvy[sloupec, radek];
+                    int NovaCervena = 255 - docasnaBarva.R;
+                    int NovaZelena = 255 - docasnaBarva.G;
+                    int NovaModra = 255 - docasnaBarva.B;
+                    Color invertovanaBarva = Color.FromArgb(NovaCervena, NovaZelena, NovaModra);
+                    obrazek.SetPixel(sloupec, radek, invertovanaBarva);
+                    barvy[sloupec, radek] = invertovanaBarva;
+                }
+            }
+            this.Invalidate();
+        }
+        private void Zesvetli(int hodnota)
+        {
+            for (int sloupec = 0; sloupec < obrazek.Width; sloupec++)
+            {
+
+                for (int radek = 0; radek < obrazek.Height; radek++)
+                {
+                    Color docasnaBarva = barvy[sloupec, radek];
+                    int NovaCervena = Math.Min(docasnaBarva.R + hodnota, 255);
+                    int NovaZelena = Math.Min(docasnaBarva.G + hodnota, 255);
+                    int NovaModra = Math.Min(docasnaBarva.B + hodnota, 255);
+                    Color zesvetlenaBarva = Color.FromArgb(NovaCervena, NovaZelena, NovaModra);
+                    obrazek.SetPixel(sloupec, radek, zesvetlenaBarva);
+                    barvy[sloupec, radek] = zesvetlenaBarva;
+                }
+            }
+            this.Invalidate();
+        }
+        private void Ztmav(int hodnota)
+        {
+            for (int sloupec = 0; sloupec < obrazek.Width; sloupec++)
+            {
+
+                for (int radek = 0; radek < obrazek.Height; radek++)
+                {
+                    Color docasnaBarva = barvy[sloupec, radek];
+                    int NovaCervena = Math.Max(docasnaBarva.R - hodnota, 0);
+                    int NovaZelena = Math.Max(docasnaBarva.G - hodnota, 0);
+                    int NovaModra = Math.Max(docasnaBarva.B - hodnota, 0);
+                    Color ztmavenaBarva = Color.FromArgb(NovaCervena, NovaZelena, NovaModra);
+                    obrazek.SetPixel(sloupec, radek, ztmavenaBarva);
+                    barvy[sloupec, radek] = ztmavenaBarva;
+                }
+            }
+            this.Invalidate();
+        }
+        private void Rozmaz()
+        {
+            for (int sloupec = 0; sloupec < obrazek.Width; sloupec++)
+            {
+
+                for (int radek = 0; radek < obrazek.Height; radek++)
+                {
+                    Color barva;
+                    if (sloupec > 0)
+                    {
+                        barva = barvy[sloupec - 1, radek];
+                    }
+                    else
+                    {
+                        barva = barvy[sloupec, radek];
+                    }
+                    int predchoziCervena = barva.R;
+                    int predchoziZelena = barva.G;
+                    int predchoziModra = barva.B;
+                    if (sloupec < obrazek.Width - 1)
+                    {
+                        barva = barvy[sloupec + 1, radek];
+                    }
+                    else
+                    {
+                        barva = barvy[sloupec, radek];
+                    }
+                    int nasledujiciCervena = barva.R;
+                    int nasledujiciZelena = barva.G;
+                    int nasledujiciModra = barva.B;
+                    int novaCervena = (predchoziCervena + nasledujiciCervena) / 2;
+                    int novaZelena = (predchoziZelena + nasledujiciZelena) / 2;
+                    int novaModra = (predchoziModra + nasledujiciModra) / 2;
+
+                    Color novaBarva = Color.FromArgb(novaCervena, novaZelena, novaModra);
+                    obrazek.SetPixel(sloupec, radek, novaBarva);
+                    barvy[sloupec, radek] = novaBarva;
+                }
+            }
+            this.Invalidate();
+        }
+        private void OtocVodorovne()
+        {
+            Color[,] docasnePole = new Color[obrazek.Width, obrazek.Height];
+            for (int sloupec = 0; sloupec < obrazek.Width; sloupec++)
+            {
+
+                for (int radek = 0; radek < obrazek.Height; radek++)
+                {
+                    obrazek.SetPixel(sloupec, radek, barvy[(obrazek.Width - 1) - sloupec, radek]);
+                    docasnePole[sloupec,radek]= barvy[(obrazek.Width - 1) - sloupec, radek];
+                }
+            }
+            barvy = docasnePole;
+            this.Invalidate();
+        }
+        private void OtocSvisle()
+        {
+            Color[,] docasnePole = new Color[obrazek.Width, obrazek.Height];
+            for (int sloupec = 0; sloupec < obrazek.Width; sloupec++)
+            {
+
+                for (int radek = 0; radek < obrazek.Height; radek++)
+                {
+                    obrazek.SetPixel(sloupec, radek, barvy[sloupec, (obrazek.Height - 1) - radek]);
+                    docasnePole[sloupec, radek] = barvy[sloupec, (obrazek.Height - 1) - radek];
+                }
+            }
+            barvy = docasnePole;
+            this.Invalidate();
+        }
+        private void tlacitko_proved_Click(object sender, EventArgs e)
+        {
+            this.zvolenyEfekt();
+            tlacitko_ulozit.Enabled = true;
+        }
+
+        private void seznamEfektu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tlacitko_proved.Enabled = true;
+            switch(seznamEfektu.SelectedIndex)
+            {
+                case 0:
+                    zvolenyEfekt = this.InverzeBarev;
+                    this.posuvnik.Visible = false;
+                    break;
+                case 1:
+                    zvolenyEfekt = this.Rozmaz;
+                    this.posuvnik.Visible = false;
+                    break;
+                case 2:
+                    zvolenyEfekt = () => this.Zesvetli(posuvnik.Value);
+                    this.posuvnik.Visible = true;
+                    break;
+                case 3:
+                    zvolenyEfekt = () => this.Ztmav(posuvnik.Value);
+                    this.posuvnik.Visible = true;
+                    break;
+                case 4:
+                    zvolenyEfekt = this.OtocVodorovne;
+                    break;
+                case 5:
+                    zvolenyEfekt = this.OtocSvisle;
+                    break;
+            }
+        }
+        delegate void DelegatZvolenyEfekt();
+        DelegatZvolenyEfekt zvolenyEfekt;
+
+        private void tlacitko_ulozit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog ulozeni = new SaveFileDialog();
+                ulozeni.DefaultExt = "jpg";
+                ulozeni.Filter = "Obrázek JPEG .jpg, .jpeg | *.jpg; *.jpeg|Bitmap .bmp | *.bmp";
+                ulozeni.AddExtension = true;
+                ulozeni.FileName = "obrazek";
+                ulozeni.ShowDialog();
+                string cesta = ulozeni.FileName;
+                if (cesta != "")
+                {
+                    obrazek.Save(cesta);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Chyba při ukládání souboru!", "Chyba");
+            }
+        }
+    }
+}
